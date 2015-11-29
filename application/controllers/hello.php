27dbd3643d3c4bf9c8bb01ec3360f448993c435e
NBSUT <2484611837@qq.com>
2015-11-29 23:35:32 +0800

    creat qrcode

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -1,27 +1,33 @@
 <?php
 class Hello extends CI_Controller{
-    public function index(){
+    public function __construct() {
+        parent::__construct();
         $this->load->library('wxsendmsg');
-        $this->wxsendmsg->getWxAccessToken();
-		//获得参数 signature nonce token timestamp echostr
-//		$nonce     = $_GET['nonce'];
-//		$token     = 'gzlhsh';
-//		$timestamp = $_GET['timestamp'];
-//		$echostr   = $_GET['echostr'];
-//		$signature = $_GET['signature'];
-//		//形成数组，然后按字典序排序
-//		$array = array();
-//		$array = array($nonce, $timestamp, $token);
-//		sort($array);
-//		//拼接成字符串,sha1加密 ，然后与signature进行校验
-//		$str = sha1( implode( $array ) );
-//		if( $str  == $signature && $echostr ){
-//			//第一次接入weixin api接口的时候
-//			echo  $echostr;
-//			exit;
-//		}else{
-//			$this->reponseMsg();
-//		}
+        //$this->wxsendmsg->getWxAccessToken();die;
+    }
+    public function index(){          
+                                    $result = $this->wxsendmsg->create_qrcode();
+		if($result['code'] != 1)
+                                            echo $result['message'];die;
+                                    //获得参数 signature nonce token timestamp echostr
+		$nonce     = $_GET['nonce'];
+		$token      = TOKEN;
+		$timestamp = $_GET['timestamp'];
+		$echostr   = $_GET['echostr'];
+		$signature = $_GET['signature'];
+		//形成数组，然后按字典序排序
+		$array = array();
+		$array = array($nonce, $timestamp, $token);
+		sort($array);
+		//拼接成字符串,sha1加密 ，然后与signature进行校验
+		$str = sha1( implode( $array ) );
+		if( $str  == $signature && $echostr ){
+			//第一次接入weixin api接口的时候
+			echo  $echostr;
+			exit;
+		}else{
+			$this->reponseMsg();
+		}
 	}
 	// 接收事件推送并回复
 	public function reponseMsg(){
--- application/libraries/Wxsendmsg.php
+++ application/libraries/Wxsendmsg.php
@@ -7,11 +7,23 @@
  *$result = $this->pay($postObj->FromUserName);
  * 
  */
-require 'mawxconfig.php';
+require 'mywxconfig.php';
+//获取access_token
 class Wxsendmsg{
     function getWxAccessToken(){
 		//1.请求url地址
-		$appid = APPID;
+                                   $mem = new Memcache(); //创建Memcache对象  
+		$mem->connect('127.0.0.1', 11211); //连接Memcache服务器
+		if(!($data=$mem->get(md5("access_token")))){
+                                                    $data = $this->getWxAccessTokenBycurl();
+                                                    $mem->set(md5("access_token"), $data, 0, $data['expires_in']);
+		}
+		return $data['access_token'];
+    }
+    //通过CURL获取access_token
+    function getWxAccessTokenBycurl()
+    {
+                                   $appid = APPID;
 		$appsecret =  APPSECRET;
 		$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$appsecret;
 		//2初始化
@@ -26,9 +38,115 @@ class Wxsendmsg{
 		}
                                     //5.关闭curl
 		curl_close( $ch );
-		$arr = json_decode($res, true);
-		var_dump( $arr );
+		return  json_decode($res, true);
     }
-    
+    //回复单文本
+    public function responseText($postObj,$content){
+		$template = "<xml>
+		<ToUserName><![CDATA[%s]]></ToUserName>
+		<FromUserName><![CDATA[%s]]></FromUserName>
+		<CreateTime>%s</CreateTime>
+		<MsgType><![CDATA[%s]]></MsgType>
+		<Content><![CDATA[%s]]></Content>
+		</xml>";
+		//注意模板中的中括号 不能少 也不能多
+		$fromUser = $postObj->ToUserName;
+		$toUser   = $postObj->FromUserName; 
+		$time     = time();
+		$msgType  = 'text';
+		echo sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+	}
+                //回复用户关注事件
+	public function responseSubscribe($postObj, $arr){
+		$this->responseNews($postObj,$arr);
+	}
+                    //取消用户关注事件
+	public function noSubscribe($postObj, $arr){
+		
+	}
+                  //获取毫秒时间戳
+                  function get_time(){
+                    $time = explode ( " ", microtime () );  
+                    $time = $time [1] . ($time [0] * 1000);  
+                    $time2 = explode ( ".", $time );  
+                    return  $time2 [0];
+                  }
+                //生成带参数的二维码
+                public function create_qrcode(){
+                    $access_token = $this->getWxAccessToken();
+                    $scene_id = $this->get_time();
+                    $qrcode = '{"expire_seconds": 604800, "action_name": "QR_SCENE", "action_info": {"scene": {"scene_id": '.$scene_id.'}}}';
+                    $ticket_url = 'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token='.$access_token;
+                    $result = $this->https_curl($ticket_url, $qrcode);
+                    $info = json_decode($result, TRUE);
+                    if(!isset($info['ticket'])){
+                        return array('code' => -1, 'message' => '获取ticket失败');
+                    }
+                    $image_url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket='.urlencode($info['ticket']);
+                    $imageinfo = $this->https_curl($image_url);
+                    $filename = getcwd().'/qrcode/'.$scene_id.'.jpg';
+                    $local_file = fopen($filename, 'w');
+                    if(false !== $local_file){
+                        if(false !== fwrite($local_file, $imageinfo)){
+                            fclose($local_file);
+                            return array('code' => 1, 'url' => $filename, 'scene_id' => $scene_id);
+                        }
+                        return array('code'=>-3, 'message' => '写入文件失败' );
+                    }
+                    return array('code'=>-2, 'message' => '创建文件失败' );
+                }
+                //CURL 获取数据
+                function https_curl($url, $data = null){
+                    $curl = curl_init();
+                    curl_setopt($curl, CURLOPT_URL, $url);
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+                    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+                    if(!empty($data)){
+                        curl_setopt($curl, CURLOPT_POST, 1);
+                        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                    }else{
+                        curl_setopt($curl, CURLOPT_HEADER, 0);
+                        curl_setopt($curl, CURLOPT_NOBODY, 0);
+                    }
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                    $output = curl_exec($curl);
+                    curl_close($curl);
+                    return $output;
+                }
+                //回复图片消息
+                public function send_qrcode($object, $media_id){
+                    $template = "<xml>
+                                        <ToUserName><![CDATA[%s]]></ToUserName>
+                                        <FromUserName><![CDATA[%s]]></FromUserName>
+                                        <CreateTime>%s</CreateTime>
+                                        <MsgType><![CDATA[%s]]></MsgType>
+                                        <Image>
+                                        <MediaId><![CDATA[%s]]></MediaId>
+                                        </Image>
+                                        </xml>";
+		//注意模板中的中括号 不能少 也不能多
+		$fromUser = $postObj->ToUserName;
+		$toUser   = $postObj->FromUserName;
+		$time     = time();
+		$msgType  = 'image';
+		echo sprintf($template, $toUser, $fromUser, $time, $msgType, $media_id);
+                }
+                //上传二维码到临时素材
+                public function add_qrcode($filepath){
+                    $type = "image";
+                    $filedata = array("file1"  => "@".$filepath);
+                    $url = "http://file.api.weixin.qq.com/cgi-bin/media/upload?access_token=$access_token&type=$type";
+                    $result = https_curl($url, $filedata);
+                    $info = json_decode($result, TRUE);
+                    if(!isset($info['media_id'])){
+                        return array('code'=>-1, '上传失败');
+                    }
+                    return array('code'=>1, 'media_id'=>$info['media_id']);
+                }
+                //从素材库中获取二维码
+                public function get_qrcode($media_id){
+                    
+                    
+                }
 }
 
